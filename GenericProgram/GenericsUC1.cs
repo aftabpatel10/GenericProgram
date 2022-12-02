564cdc07c66e0555015fe5b18ca7b8a3647ae6ac
Patel <aftabkpatel10@gmail.com>
2022-12-02 18:01:34 +0530

    Finding The Maximum Value

---
+++ GenericProgram/GenericsUC1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProgram
+{
+    public class FindMaximum
+    {
+        //UC1 finding maximum value of 3 numbers.
+        public static void FindMaxValue(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+        //UC2 finding maximum value out of 3 float.
+        public static void FindMaxValue(float first, float second, float third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+        //UC2 finding maximum value out of 3 string.
+        public static void FindMaxValue(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+    }
+}
---
+++ GenericProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Use Case Programs ");
+            //UC1 finding max out of 3 int.
+            FindMaximum.FindMaxValue(10, 20, 30);
+            //UC2 finding max out of 3 float.
+            FindMaximum.FindMaxValue(20.20f, 30.20f, 40.40f);
+            //UC3 finding max out of 3 string.
+            FindMaximum.FindMaxValue("abc", "def", "opq");
+        }
+    }
+}
