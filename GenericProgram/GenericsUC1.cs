6371076051ed0c55ce299d9e052d745952aa281b
Patel <aftabkpatel10@gmail.com>
2022-12-02 18:41:01 +0530

    Finding Maximum Float Value

--- GenericProgram/GenericsUC1.cs
+++ GenericProgram/GenericsUC1.cs
@@ -26,6 +26,24 @@ namespace GenericProgram
             else
                 Console.WriteLine("All Numbers are Equal! ");
         }
+            //UC2 finding maximum value out of 3 float.
+        public static void FindMaxValue(float first, float second, float third)
+        {
+                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+                {
+                    Console.WriteLine($"{first} is greater");
+                }
+                else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+                {
+                    Console.WriteLine($"{second} is greater");
+                }
+                else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+                {
+                    Console.WriteLine($"{third} is greater ");
+                }
+                else
+                    Console.WriteLine("All Numbers are Equal! ");
+        }
        
     }
 }
--- GenericProgram/Program.cs
+++ GenericProgram/Program.cs
@@ -13,7 +13,8 @@ namespace GenericProgram
             Console.WriteLine("Welcome to Generic Use Case Programs ");
             //UC1 finding max out of 3 int.
             FindMaximum.FindMaxValue(10, 20, 30);
-           
+            //UC2 finding max out of 3 float.
+            FindMaximum.FindMaxValue(20.20f, 30.20f, 40.40f);
         }
     }
 }
