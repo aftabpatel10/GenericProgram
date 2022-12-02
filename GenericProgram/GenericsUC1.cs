ad75fd4b7a1f5c4429353610c3eed170a32e424e
Patel <aftabkpatel10@gmail.com>
2022-12-02 18:44:22 +0530

    Finding Maximum Of Three Strings

--- GenericProgram/GenericsUC1.cs
+++ GenericProgram/GenericsUC1.cs
@@ -44,6 +44,24 @@ namespace GenericProgram
                 else
                     Console.WriteLine("All Numbers are Equal! ");
         }
-       
+        //UC3 finding maximum value out of 3 string.
+        public static void FindMaxValue(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+
     }
 }
--- GenericProgram/Program.cs
+++ GenericProgram/Program.cs
@@ -15,6 +15,8 @@ namespace GenericProgram
             FindMaximum.FindMaxValue(10, 20, 30);
             //UC2 finding max out of 3 float.
             FindMaximum.FindMaxValue(20.20f, 30.20f, 40.40f);
+            //UC3 finding max out of 3 string.
+            FindMaximum.FindMaxValue("abc", "def", "opq");
         }
     }
 }
