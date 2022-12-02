e6493821a9b1e431d7d4172dd38a854eeb4e065f
Patel <aftabkpatel10@gmail.com>
2022-12-02 18:34:41 +0530

    Finding Maximum Values From Given Numbers

--- GenericProgram/Program.cs
+++ GenericProgram/Program.cs
@@ -13,10 +13,7 @@ namespace GenericProgram
             Console.WriteLine("Welcome to Generic Use Case Programs ");
             //UC1 finding max out of 3 int.
             FindMaximum.FindMaxValue(10, 20, 30);
-            //UC2 finding max out of 3 float.
-            FindMaximum.FindMaxValue(20.20f, 30.20f, 40.40f);
-            //UC3 finding max out of 3 string.
-            FindMaximum.FindMaxValue("abc", "def", "opq");
+           
         }
     }
 }
